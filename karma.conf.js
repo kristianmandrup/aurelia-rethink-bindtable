9d2ca6ca805b5a1bd81e886b58080b576a366e6a
pablogq <chino7.17@gmail.com>
2015-03-11 16:43:09 -0600

    fixing karma configuration
    karma-jspm is being used so karma-babel-preprocessor is not necessary
    anymore as karma-jspm handles it.
    
    The karma babel preprocessor was creating a conflict with karma-jspm,
    so I removed babel configuration in karma and uninstalled the npm
    package.
    
    Also, for some reason the connect function of the exported object in
    socket-io mock was undefined, so I created the object with the function
    inside of the definition and export the instance of that object, now it
    seems to be working.

--- karma.conf.js
+++ karma.conf.js
@@ -36,17 +36,7 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'test/**/*.js': ['babel'],
-      'src/**/*.js': ['babel']
-    },
-    '6to5Preprocessor': {
-      options: {
-        sourceMap: 'inline',
-        modules: 'system',
-        moduleIds: false
-      }
-    },
+    preprocessors: { },
 
 
     // test results reporter to use
